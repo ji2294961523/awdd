decf80f92ad19e904c413faabc37041a0e12a482
ji2294961523 <ji2294961523@qq.com>
2018-10-23 19:23:01 +0800

    115511

--- wowowo/mygame.js
+++ wowowo/mygame.js
@@ -1,24 +1,26 @@
-var randomNumber=Math.floor((Math.random()*100));
+var randomNumber=Math.floor(Math.random()*100)+1;
 // console.log(randomNumber);
 
-var guessField=document.querySelector('.guessField');
+var guessField=document.querySelector('.guessField');//从文本框获取数 通过class来获得
  console.log(guessField);
-var guessField=document.querySelector('.guesses');
-console.log(guesses);
+//  console.log(guessField.Value);
+var guessSubmit=document.querySelector('.guessSubmit');
+//  console.log(guessSubmit);
+//  console.log(guessSubmit.Value)
+var guesses=document.querySelector('.guesses');
+console.log(guesses)
 var lastResult=document.querySelector('.lastResult');
 var lowOrHi=document.querySelector('.lowOrHi');
-var guessSubmit=document.querySelector('.guessSubmit');
- 
-var guessesCoutn=1;
-var resetButton;
-guessField.focus();
 
-function checkGuess() //形式参数
-{
- guesses.textContent += guessField.value+" ";
- guesses.style.backgroundColor="red";
+var guessCoutn=1;//获取随机数
+var resetButtom;
+guessField.focus(); //获得焦点
 
+function checkGues(){  //方法体 关键字function
+    guesses.innerHTML+=guessField.value+" "; //返回输入的text文本框的内容
+    guesses.style.backgroundColor="red";  //背景颜色
 }
+//checkGues(); //调用方法
 
-// checkGuess();实际参数
-guessSubmit.addEventListener('click',checkGuess);
+guessSubmit.addEventListener('click',checkGues);
+
